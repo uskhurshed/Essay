a2d812e943d9857a31d44d0c15593bb57c4fe189
khurshed <khurshed.apps@gmail.com>
2025-01-08 11:26:30 +0500

    ---

--- app/src/main/java/com/easyapps/zkplayer/fragments/ContentFragment.kt
+++ app/src/main/java/com/easyapps/zkplayer/fragments/ContentFragment.kt
@@ -1,32 +1,27 @@
 package com.easyapps.zkplayer.fragments
 
-import android.content.Context
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.easyapps.zkplayer.databinding.FragmentContentBinding
 import com.easyapps.zkplayer.utils.TempDB
+import com.easyapps.zkplayer.utils.WindowUtils.setupBottomInsets
+import com.easyapps.zkplayer.utils.WindowUtils.setupTopInsets
 import com.easyapps.zkplayer.utils.ads.BannerAds
 
 class ContentFragment : Fragment() {
 
-    companion object {
-        private const val MAX_TEXT_SIZE = 55f
-        private const val MIN_TEXT_SIZE = 16f
-        private const val DEFAULT_TEXT_SIZE = 16f
-    }
-
     private lateinit var binding: FragmentContentBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentContentBinding.inflate(inflater, container, false)
+        binding.toolbar.setupTopInsets()
+        binding.detailAboutText.setupBottomInsets()
         return binding.root
     }
 
@@ -35,15 +30,14 @@ class ContentFragment : Fragment() {
 
         detailTitleText.text = arguments?.getString("title").orEmpty()
         detailAboutText.text = arguments?.getString("content").orEmpty()
-        detailAboutText.textSize = DEFAULT_TEXT_SIZE
 
         BannerAds(requireContext(), forad, TempDB.adsBanner)
         aPlus.setOnClickListener {
-            adjustTextSize(increment = true)
+            detailAboutText.changeTextSize(increase = true)
         }
 
         aMinus.setOnClickListener {
-            adjustTextSize(increment = false)
+            detailAboutText.changeTextSize(increase = false)
         }
 
         toolbar.setOnClickListener {
@@ -51,14 +45,10 @@ class ContentFragment : Fragment() {
         }
     }
 
-
-    private fun FragmentContentBinding.adjustTextSize(increment: Boolean) {
-        val currentSize = detailAboutText.textSize
-        val newSize = if (increment) (currentSize + 1f).coerceAtMost(MAX_TEXT_SIZE)
-        else (currentSize - 1f).coerceAtLeast(MIN_TEXT_SIZE)
-        detailAboutText.textSize = newSize
+    private fun TextView.changeTextSize(increase: Boolean, minSize: Float = 12f, maxSize: Float = 30f) {
+        val currentSize = textSize / (resources.displayMetrics.density * resources.configuration.fontScale)
+        val newSize = if (increase) currentSize + 1 else currentSize - 1
+        val clampedSize = newSize.coerceIn(minSize, maxSize)
+        this.setTextSize(TypedValue.COMPLEX_UNIT_SP, clampedSize)
     }
-
-    fun  Int.dp(context: Context) = (this * context.resources.displayMetrics.density).toInt()
-
 }
